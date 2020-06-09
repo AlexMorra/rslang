f00105fca221976a74a6d56817eb7182397cda75
Alexandr Kartynnik <k77.wolf@gmail.com>
2020-06-09 12:47:45 +0300

    feat: add user login handler

--- src/app.js
+++ src/app.js
@@ -5,47 +5,21 @@ import './assets/js/account'
 import {Auth} from "./assets/js/auth";
 
 // INIT
-window.current_page = '';
+window.current_page = null;
 let auth = new Auth();
 // auth.login_user('k77.wolf@gmail.com', '1234_Wolf').then(response=> console.log(response))
-// auth.get_user()
-// if (!auth.get_user()) {
-// }
+
+console.log(auth.authorized())
+console.log(typeof auth.authorized())
 
 
-// fetch('http://127.0.0.1:8000/api/v1/movie/1/',{
-//     method: 'GET'
-// })
-// .then(response => response.json())
-// .then(response_json => console.log(response_json))
+if (typeof auth.authorized() !== 'object' && window.current_page !== 'auth') {
+    auth.show_login_page();
+}
 
-get_user('k77.wolf@gmail.com', '1234_Wolf');
-
-    function get_user() {
-        let token = localStorage.getItem('token');
-        let user_id = localStorage.getItem('user_id');
-        return fetch(`https://afternoon-falls-25894.herokuapp.com/users/${user_id}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(response => {
-                if (response.ok) return response.json();
-                if (response.status === 401) {
-                    console.log('Access token is missing or invalid')
-                } else if (response.status === 404) {
-                    console.log('Settings not found.')
-                } else {
-                    console.log('User not found')
-                }
-                return false
-            })
-            .then(response_json => {
-                console.log(response_json);
-                return true
-            })
-            .catch(error => console.log(error))
+setInterval(() => {
+    if (typeof auth.authorized() !== 'object' && window.current_page !== 'auth') {
+        auth.show_login_page();
+        console.log('REDIRECT TO LOGIN PAGE');
     }
+}, 10000);
--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,24 +1,68 @@
-let create_password_input = document.querySelector('.user-password');
-let show_password_btn = document.querySelector('.show-password');
-
 export class Auth {
     constructor() {
         this.body = document.querySelector('body');
         this.login_page = document.getElementById('login-template');
-        this.create_page = document.getElementById('create-template')
+        this.create_page = document.getElementById('create-template');
     }
     show_login_page() {
-        this.body.prepend(this.login_page.content.cloneNode(true))
+        window.current_page = 'auth';
+        this.body.prepend(this.login_page.content.cloneNode(true));
+        let password_visibility_btn = document.querySelector('.show-password');
+        let login_user_btn = document.getElementById('login-user');
+        password_visibility_btn.addEventListener('click', this.password_visibility);
+        login_user_btn.addEventListener('click', this.login_handler.bind(this));
     }
 
     show_create_page() {
         this.body.prepend(this.create_page.content.cloneNode(true))
     }
 
-    get_user() {
+    login_handler(e) {
+        console.log('IN');
+        e.preventDefault();
+        let input_login = document.getElementById('login-email');
+        let input_password = document.getElementById('login-password');
+        let email = input_login.value;
+        let password = input_password.value;
+        if (this.email_validator(email) && this.password_validator(password)) {
+            console.log('validate true');
+            this.login_user(email, password).then(response => {
+                console.log(response);
+                localStorage.setItem('token', response.token);
+                localStorage.setItem('user_id', response.userId);
+                this.login_success();
+            })
+        }
+    }
+
+    login_user(email, password) {
+        let user = JSON.stringify({'email': email, 'password': password});
+        return fetch('https://afternoon-falls-25894.herokuapp.com/signin', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: user
+        })
+            .then(response => {
+                if (!response.ok) { throw Error(response.statusText) }
+                return response.json()
+            })
+            .then(response_json => response_json)
+            .catch(error => console.log(error))
+    }
+
+    login_success() {
+        window.current_page = null;
+        let user_login_page = document.querySelector('.user-login-page');
+        user_login_page.remove();
+    }
+
+    authorized() {
         let token = localStorage.getItem('token');
         let user_id = localStorage.getItem('user_id');
-        if (!token && !user_id) return false;
+        if (!token || !user_id) return false;
         return fetch(`https://afternoon-falls-25894.herokuapp.com/users/${user_id}`, {
             method: 'GET',
             headers: {
@@ -29,19 +73,10 @@ export class Auth {
         })
             .then(response => {
                 if (!response.ok) { throw Error(response.statusText) }
-                return response
-                // if (response.ok) return response.json();
-                // if (response.status === 401) {
-                //     console.log('Access token is missing or invalid')
-                // } else if (response.status === 404) {
-                //     console.log('Settings not found.')
-                // } else {
-                //     console.log('User not found')
-                // }
-                // return false
+                return response.json()
             })
             .then(response_json => {
-                return console.log(response_json)
+                return response_json
             })
             .catch(error => console.log(error))
     }
@@ -60,45 +95,54 @@ export class Auth {
             .then(response_json => response_json);
     }
 
-    login_user(email, password) {
-        let user = JSON.stringify({'email': email, 'password': password});
-        return fetch('https://afternoon-falls-25894.herokuapp.com/signin', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: user
-        })
-            .then(response => {
-                if (response.ok) return response.json();
-                if (response.status === 403) {
-                    console.log('Incorrect e-mail or password.')
-                } else {
-                    console.log('Something went wrong.')
-                }
-                return false
-            })
-            .then(response_json => {
-                localStorage.setItem('token', response_json.token);
-                localStorage.setItem('user_id', response_json.userId);
-                return true
-            })
-            .catch(error => console.log(error))
+    password_visibility() {
+        console.log('PASSWORD VISIBILITY');
+        let password_visibility_btn = document.querySelector('.show-password');
+        let login_password_input = document.getElementById('login-password');
+
+        let input_type = login_password_input.getAttribute('type');
+        if (input_type === 'password') {
+            login_password_input.setAttribute('type', 'text');
+            password_visibility_btn.classList.remove('fa-eye-slash');
+            password_visibility_btn.classList.add('fa-eye')
+        } else if (input_type === 'text') {
+            login_password_input.setAttribute('type', 'password');
+            password_visibility_btn.classList.add('fa-eye-slash');
+            password_visibility_btn.classList.remove('fa-eye')
+        }
+    }
+
+    password_validator(password) {
+        let error_messages = [];
+        let message_container = document.querySelector('.error-message');
+        message_container.innerHTML = '';
+        if (password.length < 8) {
+            let message = document.createElement('li');
+            message.textContent = 'Используйте не менее 8 символов.';
+            error_messages.push(message)
+        }
+        if (!password.match(/a-z/g) && !password.match(/A-Z/g) && !password.match(/[0-9]/g)) {
+            let message = document.createElement('li');
+            message.textContent = 'Используйте строчные и прописные латинские буквы (a-z, A-Z) и цифры.';
+            error_messages.push(message);
+        }
+        if (error_messages.length) {
+            let password_icon = document.querySelector('.auth svg');
+            password_icon.classList.add('shake');
+            setTimeout(() => password_icon.classList.remove('shake'), 500);
+            error_messages.forEach(message => message_container.append(message));
+            return false
+        }
+        return true
+    }
+
+    email_validator(email) {
+        let email_icon = document.querySelector('.auth .fa-envelope');
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(String(email).toLowerCase())) {
+            email_icon.classList.add('shake');
+            setTimeout(() => email_icon.classList.remove('shake'), 500);
+        }
+        return re.test(String(email).toLowerCase())
     }
 }
-
-
-// password visibility
-// show_password_btn.addEventListener('click', (e) => {
-//     let input_type = create_password_input.getAttribute('type');
-//     if (input_type === 'password') {
-//         create_password_input.setAttribute('type', 'text');
-//         show_password_btn.classList.remove('fa-eye-slash');
-//         show_password_btn.classList.add('fa-eye')
-//     } else if (input_type === 'text') {
-//         create_password_input.setAttribute('type', 'password');
-//         show_password_btn.classList.add('fa-eye-slash');
-//         show_password_btn.classList.remove('fa-eye')
-//     }
-// });
