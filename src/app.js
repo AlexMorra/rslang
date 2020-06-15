03cc85abbc0e5588f3a0daf1402fa56d04c3fb05
Eugenia Shibkova <shibkova91@gmail.com>
2020-06-15 05:08:08 +0300

    refactor: refactor Menu class param

--- src/app.js
+++ src/app.js
@@ -7,30 +7,31 @@ import State from './js/usersAppState';
 import Menu from './js/menu';
 import ControlPanel from './js/controlPanel/controlPanel';
 import Account from './js/account';
+import GamesPage from './js/gamesPage';
 
 // INIT
 window.currentPage = null;
 let auth = new Auth();
 
-export let menu = new Menu(new ControlPanel(), new Account(), new Auth());
+export let menu = new Menu(new ControlPanel(), new Account(), new Auth(), new GamesPage());
 export let usersAppState = new State();
 
-// // check if user has session and load settings if has
-// auth.authorized().then(authorized => {
-//   if (!authorized && window.currentPage !== 'auth') {
-//     auth.showLoginPage();
-//   } else {
-//     auth.loginSuccess();
-//   }
-// }).finally(() => {
-//   // set the session check every 10 seconds
-//   setInterval(() => {
-//     if (window.currentPage !== 'auth') {
-//       auth.authorized().then(authorized => {
-//         if (!authorized) {
-//           auth.showLoginPage();
-//         }
-//       });
-//     }
-//   }, 10000);
-// });
+// check if user has session and load settings if has
+auth.authorized().then(authorized => {
+  if (!authorized && window.currentPage !== 'auth') {
+    auth.showLoginPage();
+  } else {
+    auth.loginSuccess();
+  }
+}).finally(() => {
+  // set the session check every 10 seconds
+  setInterval(() => {
+    if (window.currentPage !== 'auth') {
+      auth.authorized().then(authorized => {
+        if (!authorized) {
+          auth.showLoginPage();
+        }
+      });
+    }
+  }, 10000);
+});
