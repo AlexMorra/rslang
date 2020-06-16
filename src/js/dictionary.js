e8297e8b12a325c3995e72662332874025391cc0
Alexandr Kartynnik <k77.wolf@gmail.com>
2020-06-16 21:55:19 +0300

    feat: add remove words from dictionary

--- src/js/dictionary.js
+++ src/js/dictionary.js
@@ -4,11 +4,13 @@ import wordCards from './wordCards';
 export default class Dictionary {
   constructor() {
     this.mainArea = document.querySelector('.main-area');
+    this.checkedWordsId = null;
+    this.chekedWords = null;
     this.wordListWrapper = null;
     this.header = null;
     this.chooseToggleBtn = null;
     this.inputWordSearch = null;
-    this.addToDictionaryBtn = null;
+    this.deleteWordsBtn = null;
     this.audio = null;
     this.currentWords = [];
   }
@@ -17,8 +19,55 @@ export default class Dictionary {
     setTimeout(() => {
       let wordList = this.createWordList();
       this.mainArea.append(wordList);
+      wordList.addEventListener('click', this.wordListHandler.bind(this));
       this.inputWordSearch.addEventListener('input', this.wordSearchHandler.bind(this));
+      this.chooseToggleBtn.addEventListener('change', this.chooseToggle);
+      this.deleteWordsBtn.addEventListener('click', this.deleteWords.bind(this));
     }, 400);
+  }
+
+  wordListHandler(e) {
+    if (e.target.getAttribute('type') === 'checkbox') {
+      if (e.target === this.chooseToggleBtn) this.chooseToggle(e);
+      this.checkedWordsId = [...this.wordListWrapper.querySelectorAll('.word-checkbox-delete')]
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox=> checkbox.getAttribute('id'));
+      console.log(this.checkedWordsId, 'CHECKED WORDS');
+      if (this.checkedWordsId.length) {
+        this.inputWordSearch.style.display = 'none';
+        this.deleteWordsBtn.removeAttribute('style');
+      } else {
+        this.inputWordSearch.removeAttribute('style');
+        this.deleteWordsBtn.style.display = 'none';
+      }
+    } else if (e.target.dataset.audio) {
+      this.audio.src = `./assets/${e.target.dataset.src}`;
+      this.audio.play();
+    }
+  }
+
+  chooseToggle(e) {
+    this.chekedWords = [...document.querySelectorAll('.word-checkbox-delete')];
+    if (e.target.checked) {
+      this.chekedWords.forEach(checkbox => checkbox.checked = true);
+    } else {
+      this.chekedWords.forEach(checkbox => checkbox.checked = false);
+    }
+  }
+
+  deleteWords(e) {
+    e.preventDefault();
+    this.checkedWordsId.forEach(wordId => {
+      let wordElement = document.querySelector(`[data-word-id="${wordId}"]`);
+      usersAppState.deleteUserWord(wordId).then(() => {
+        wordElement.nextElementSibling.remove();
+        wordElement.remove();
+        let word = usersAppState.userWords.find(word => word.wordId === wordId);
+        usersAppState.userWords.pop(word);
+      });
+    });
+    this.inputWordSearch.removeAttribute('style');
+    this.deleteWordsBtn.style.display = 'none';
   }
 
   wordSearchHandler() {
@@ -63,13 +112,13 @@ export default class Dictionary {
       <div class="word-list-header">
         <input type="checkbox" class="chooseToggle">
         <input type="text" class="word-search" placeholder="Найти">
-        <input type="submit" class="add-to-dictionary" value="Добавить в словарь" style="display: none">
+        <input type="submit" class="delete-words" value="Удалить из словаря" style="display: none">
       </div>
     `;
     this.header = this.header.content.querySelector('.word-list-header');
     this.chooseToggleBtn = this.header.querySelector('.chooseToggle');
     this.inputWordSearch = this.header.querySelector('.word-search');
-    this.addToDictionaryBtn = this.header.querySelector('.add-to-dictionary');
+    this.deleteWordsBtn = this.header.querySelector('.delete-words');
     return this.header;
   }
 
@@ -79,8 +128,8 @@ export default class Dictionary {
 
     let wordTemplate = document.createElement('template');
     wordTemplate.innerHTML = `
-      <div class="word-list-row">
-<!--        <input class="word-checkbox" type="checkbox" name="word" id="word.id">  -->
+      <div class="word-list-row" data-word-id="${word.id}">
+        <input class="word-checkbox-delete" type="checkbox" name="word" id="${word.id}">  
         <i class="fas fa-volume-up" data-audio="play" data-src="${word.audio}"></i>
         <div class="word">
             ${word.word}
