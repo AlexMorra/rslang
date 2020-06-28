7dc2b2ef261c5e07e4a169a47427fd1c3cddc21c
Eugenia Shibkova <shibkova91@gmail.com>
2020-06-29 02:43:13 +0300

    feat: add team page js

---
+++ src/js/team/teamConstants.js
@@ -0,0 +1,41 @@
+const TEAM = [
+    {
+      image: '',
+      title: 'тимлид',
+      fullname: 'Александр Картынник',
+      github: 'AlexMorra'
+    },
+    {
+      image: '',
+      title: 'фронтендер',
+      fullname: 'Дмитрий',
+      github: 'DzmitryDabrahost'
+    },
+    {
+      image: '',
+      title: 'фронтендер',
+      fullname: 'Станислав Яркин',
+      github: 'yarkin13'
+    },
+    {
+      image: '',
+      title: 'фронтендер',
+      fullname: 'Евгения Шибкова',
+      github: 'jenia-shibkova',
+    },
+    {
+      image: '',
+      title: 'фронтендер',
+      fullname: 'Алексей Ивкин',
+      github: 'ivkin-alexey'
+    },
+    {
+      image: '',
+      title: 'фронтендер',
+      fullname: 'Денис Марытчак',
+      github: 'SonOfGlory'
+    },
+];
+  
+export default GAMES;
+  
