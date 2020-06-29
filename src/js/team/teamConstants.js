f5f49ad56d67afdb2d341d01a0981a743b68de87
Eugenia Shibkova <shibkova91@gmail.com>
2020-06-29 04:17:22 +0300

    refactor: refactor project

--- src/js/team/teamConstants.js
+++ src/js/team/teamConstants.js
@@ -1,41 +1,40 @@
 const TEAM = [
-    {
-      image: '',
-      title: 'тимлид',
-      fullname: 'Александр Картынник',
-      github: 'AlexMorra'
-    },
-    {
-      image: '',
-      title: 'разработчик',
-      fullname: 'Дмитрий',
-      github: 'DzmitryDabrahost'
-    },
-    {
-      image: '',
-      title: 'разработчик',
-      fullname: 'Станислав Яркин',
-      github: 'yarkin13'
-    },
-    {
-      image: '',
-      title: 'разработчик',
-      fullname: 'Евгения Шибкова',
-      github: 'jenia-shibkova',
-    },
-    {
-      image: '',
-      title: 'разработчик',
-      fullname: 'Алексей Ивкин',
-      github: 'ivkin-alexey'
-    },
-    {
-      image: '',
-      title: 'разработчик',
-      fullname: 'Денис Марытчак',
-      github: 'SonOfGlory'
-    },
+  {
+    image: '',
+    title: 'тимлид',
+    fullname: 'Александр Картынник',
+    github: 'AlexMorra'
+  },
+  {
+    image: '',
+    title: 'разработчик',
+    fullname: 'Дмитрий',
+    github: 'DzmitryDabrahost'
+  },
+  {
+    image: '',
+    title: 'разработчик',
+    fullname: 'Станислав Яркин',
+    github: 'yarkin13'
+  },
+  {
+    image: '',
+    title: 'разработчик',
+    fullname: 'Евгения Шибкова',
+    github: 'jenia-shibkova'
+  },
+  {
+    image: '',
+    title: 'разработчик',
+    fullname: 'Алексей Ивкин',
+    github: 'ivkin-alexey'
+  },
+  {
+    image: '',
+    title: 'разработчик',
+    fullname: 'Денис Марытчак',
+    github: 'SonOfGlory'
+  }
 ];
-  
+
 export default TEAM;
-  
