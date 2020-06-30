22c79ca60902fbb96f1bb0dbd7054b6e7584b7c1
Yarkin13 <stas20072008@yandex.ru>
2020-06-30 18:49:24 +0400

    feat: add common method rendering statistic

--- src/js/games/english-puzzle/english-puzzle-main-block.class.js
+++ src/js/games/english-puzzle/english-puzzle-main-block.class.js
@@ -1,10 +1,9 @@
-import wordCards from '../../wordCards';
 import * as utils from '../../utils';
 import { usersAppState } from '../../../app';
 import { dragAndDrop, wordClick } from './drag-and-drop-and-click-word';
 import EnglishPuzzleHintsBlock from './english-puzzle-hints-block.class';
 import EnglishPuzzleButtonsBlock from './english-puzzle-buttons-block.class';
-import EnglishPuzzle from './english-puzzle';
+
 export default class EnglishPuzzleMainBlock {
   constructor() {
     this.mainArea = document.querySelector('.main-area');
@@ -140,16 +139,8 @@ export default class EnglishPuzzleMainBlock {
       wordClick();
     } else {
       this.handingStatistic();
-      this.getStatistic(this.statistic);
+      utils.getStatistic(this.statistic)
     }
-  }
-
-  getStatistic(statisticArray) {
-    utils.destroy();
-    setTimeout(() =>{
-      this.createStatistic(statisticArray);
-      this.addEventHandlerInStatistic();
-    }, 400);
   }
 
   handingStatistic() {
@@ -170,101 +161,4 @@ export default class EnglishPuzzleMainBlock {
     return this.statistic;
   }
 
-  createStatistic(statisticArray) {
-    const errors = statisticArray.reduce((acc, currentValue) => {
-      if (!currentValue.isLearned) {
-        acc += 1;
-      }
-      return acc;
-    }, 0);
-    const right = statisticArray.reduce((acc, currentValue) => {
-      if (currentValue.isLearned) {
-        acc += 1;
-      }
-      return acc;
-    }, 0);
-    const statisticNode = document.createElement('template');
-    statisticNode.innerHTML = `
-    <div class="tab-wrapper">
-      <div class="statistic">
-        <div class="statistic-wrapper">
-          <div class="statistic__learned">
-            <div class="statistic__learned__title__wrapper">
-              <p class="statistic__learned__title">Знаю</p>
-              <span class="statistic__learned__quantity">${right}</span>
-            </div>
-          </div>
-          <div class="statistic__not-learned">
-            <div class="statistic__not-learned__title__wrapper">
-              <p class="statistic__not-learned__title">Ошибок</p>
-              <span class="statistic__not-learned__quantity">${errors}</span>
-            </div>
-          </div>
-        </div>
-        <div class="statistic__btn-container">
-          <button class="statistic__btn-container__repeat">Попробовать еще раз</button>
-          <button class="statistic__btn-container__return">Вернуться к играм</button>
-        </div>
-      </div>
-      </div>
-    `.trim();
-    const learnedNode = statisticNode.content.querySelector('.statistic__learned');
-    const notLearnedNode = statisticNode.content.querySelector('.statistic__not-learned');
-    statisticArray.forEach(el => {
-      if (el.isLearned) {
-        const statisticEl = document.createElement('template');
-        statisticEl.innerHTML = `
-          <div class="statistic__el__wrapper">
-            <button id="${el.audioSrc}" class="statistic__el__audio"></button>
-            <p class="statistic__el__word">${el.word}</p>
-            <p class="statistic__el__transcription">${el.transcription}</p>
-            <p class="statistic__el__translate">${el.translate}</p>
-            <button id="${el.id}" class="statistic__el__delete" title="Удалить из словаря"></button>
-          </div>
-        `;
-        learnedNode.append(statisticEl.content);
-      } else {
-        const statisticEl = document.createElement('template');
-        statisticEl.innerHTML = `
-          <div class="statistic__el__wrapper">
-            <button id="${el.audioSrc}" class="statistic__el__audio"></button>
-            <p class="statistic__el__word">${el.word}</p>
-            <p class="statistic__el__transcription">${el.transcription}</p>
-            <p class="statistic__el__translate">${el.translate}</p>
-            <button id="${el.id}" class="statistic__el__delete" title="Удалить из словаря"></button>
-          </div>
-        `;
-        notLearnedNode.append(statisticEl.content);
-      }
-      this.mainArea.append(statisticNode.content);
-    });
-  }
-
-  addEventHandlerInStatistic() {
-    const audioBtns = document.querySelectorAll('.statistic__el__audio');
-    audioBtns.forEach(el => {
-      el.addEventListener('click', () => {
-        const audio = new Audio();
-        audio.preload = 'auto';
-        audio.src = `https://raw.githubusercontent.com/yarkin13/rslang-data/master/${el.id}`;
-        audio.play();
-      });
-    });
-    const deleteBtns = document.querySelectorAll('.statistic__el__delete');
-    deleteBtns.forEach(el => {
-      el.addEventListener('click', () => {
-        this.usersAppState.deleteUserWord(el.id, true);
-      });
-    });
-    const returnBtn = document.querySelector('.statistic__btn-container__return');
-    returnBtn.addEventListener('click', () => {
-      utils.destroy();
-      document.querySelector('#nav-games').click();
-    });
-    const repeatBtn = document.querySelector('.statistic__btn-container__repeat');
-    repeatBtn.addEventListener('click', () => {
-      utils.destroy();
-      new EnglishPuzzle().showMainPage();
-    });
-  }
 }
