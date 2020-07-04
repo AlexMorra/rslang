4956f4dbb9fe6d3d671ab2be201a67b4830c79a6
Eugenia Shibkova <shibkova91@gmail.com>
2020-07-04 17:11:13 +0300

    refactor: refactor Savanna game

--- src/js/games/savanna/savanna.js
+++ src/js/games/savanna/savanna.js
@@ -1,5 +1,6 @@
 import * as utils from '../../utils';
 import { usersAppState } from '../../../app';
+import SavannaIntro from './savannaIntro';
 
 export default class Savanna {
   constructor(state) {
@@ -8,7 +9,6 @@ export default class Savanna {
     this.cardList = null;
     this.mainArea = document.querySelector('.main-area');
     this.startButton = null;
-    this.intro = null;
     this.loader = null;
     this.gameContent = null;
     this.counter = null;
@@ -23,7 +23,8 @@ export default class Savanna {
     this.answers = null;
     this.question = null;
     this.questionWord = null;
-    this.answerWords = null;
+    this.allAnswers = null;
+    this.answersIndex = null;
     this.questionWrapper = null;
     this.currentAnswer = null;
     this.errors = 0;
@@ -35,26 +36,75 @@ export default class Savanna {
     this.errorSound = new Audio('./assets/sounds/error.mp3');
     this.successSound = new Audio('./assets/sounds/success.mp3');
     this.gameOverSound = new Audio('./assets/sounds/game-over.wav');
+    this.intro = new SavannaIntro().getElement();
+    this.statistics = [];
   }
 
   show() {
     setTimeout(() => {
-      this.start();
+      this.element = this.getElement();
+      this.element.append(this.intro);
       this.mainArea.append(this.element);
+      this.initElements();
+      this.startButtonClickHandler();
     }, 400);
+  }
+  
+  startAgain() {
+    setTimeout(() => {
+      this.element = this.getElement();
+      this.mainArea.append(this.element);
+      this.initElements();
+      this.loader.classList.toggle('none');
+
+      this.counterOn = true;
+      this.tickPlay(this.initGame.bind(this));
+      this.startBell.play();
+    }, 400);
+  }
+
+  initElements() {
+    this.startButton = this.element.querySelector('.into__button');
+    this.loader = this.element.querySelector('.loader');
+    this.counter = this.element.querySelector('.preloader__counter');
+    this.sound = this.element.querySelector('.sound');
+    this.allAnswers = [...this.element.querySelectorAll('.answer__word')];
+    this.questionWord = this.element.querySelector('.question__word');
+    this.questionWrapper = this.element.querySelector('.question');
+    this.blockAnswers = this.element.querySelector('.answers');
+    this.gameContent = this.element.querySelector('.game-wrapper');
+    this.lives = [...this.element.querySelectorAll('.heart')];
+    this.results = this.element.querySelector('.results');
+  }
+  
+  startButtonClickHandler() {
+    this.startButton.addEventListener('click', () => {
+      this.intro.remove();
+      this.loader.classList.toggle('none');
+
+      this.counterOn = true;
+      this.tickPlay(this.initGame.bind(this));
+      this.startBell.play();
+    });
+  }
+
+  initGame() {
+    setTimeout(() => {      
+      this.gameOn = true;
+      this.gameNum += 1;
+      this.generateGameData();
+      this.showGame();
+      this.checkDistance();
+      this.initHandlers();
+    }, 400); 
+
+    setTimeout((() => this.toggleFall()), 500);  
   }
 
   getElement() {
     const template = document.createElement('template');
     template.innerHTML = `
       <div class="tab-wrapper savanna">
-        <div class="intro">
-          <h1 class="intro__title">Саванна</h1>
-          <p class="intro__description">Тренировка Саванна развивает словарный запас.</br>
-            Чем больше слов ты знаешь, тем больше очков опыта получишь.</p>
-          <button class="into__button button">Начать</button>
-        </div>
-
         <div class="loader none">
           <div class="preloader">
             <p class="preloader__counter">3</p>
@@ -80,8 +130,7 @@ export default class Savanna {
 
           <div class="answers">
             <div class="answer">
-              <span class="answer__word answer__word--1"></span>
-            
+              <span class="answer__word answer__word--1"></span>            
             </div>
             <div class="answer">
               <span class="answer__word answer__word--2"></span>
@@ -133,9 +182,10 @@ export default class Savanna {
   generateGameData() {
     this.dataIndex += 4;
     this.answers = this.wordsList.slice(this.startDataIndex, this.endDataIndex);
-    this.question = this.answers[Math.floor(Math.random() * (4 - 0)) + 0];
+    this.answersIndex = Math.floor(Math.random() * (4 - 0)) + 0;
+    this.question = this.answers[this.answersIndex];
 
-    this.answerWords.forEach((element, index) => {
+    this.allAnswers.forEach((element, index) => {     
       element.textContent = this.answers[index].wordTranslate;
     });
 
@@ -165,10 +215,22 @@ export default class Savanna {
     }, 1000);
   }
 
-  playGame() {
-    this.currentAnswer.classList.remove('answer--true');
-    this.currentAnswer.classList.remove('answer--false');
+  resetAnswersState() {
+    if (this.currentAnswer) {      
+      this.currentAnswer.classList.remove('answer--true');
+      this.currentAnswer.classList.remove('answer--false');
+    }    
+    this.allAnswers[this.answersIndex].parentNode.classList.remove('answer--true');
+  }
 
+  resetQuestionState() {
+    this.questionWrapper.classList.remove('question--fail');
+    this.questionWrapper.classList.remove('question--success');
+    this.questionWrapper.classList.remove('start');
+  }
+
+  playGame() {
+    this.resetAnswersState();
     this.gameOn = true;
     this.gameNum += 1;
 
@@ -176,11 +238,9 @@ export default class Savanna {
       setTimeout(() => this.showResults(), 600);
     } else {
       setTimeout(() => {
-        this.questionWrapper.classList.remove('question--fail');
-        this.questionWrapper.classList.remove('question--success');
-        this.questionWrapper.classList.remove('start');
+        this.resetQuestionState();
         this.generateGameData();
-        this.checkOffset();
+        this.checkDistance();
 
         if (!this.questionWrapper.classList.contains('fall')) {
           this.questionWrapper.classList.add('fall');
@@ -205,6 +265,10 @@ export default class Savanna {
     }
 
     this.addSuccess();
+    this.moveBackground();
+  }
+
+  moveBackground() {    
     this.successes += 1;
     this.bgPosition -= 5;
     this.element.style.backgroundPositionY = `${this.bgPosition}%`;
@@ -217,10 +281,15 @@ export default class Savanna {
 
     this.addFail();
     this.lives[this.errors].classList.add('heart--lost');
-    this.errors += 1;   
+    this.errors += 1;
   }
 
-  checkOffset() {
+  getCorrectAnswer() {
+    console.log(this.answers)
+    console.log(this.questionWord)
+  }
+
+  checkDistance() {
     const threshold = document.documentElement.clientHeight / 2;
 
     if (this.gameOn) {
@@ -229,6 +298,7 @@ export default class Savanna {
 
         if (posY === threshold) {
           clearInterval(timerId);
+          this.allAnswers[this.answersIndex].parentNode.classList.add('answer--true');
           this.getWrongAnswer();
 
           if (this.errors === 5) {
@@ -236,7 +306,6 @@ export default class Savanna {
           } else {
             setTimeout(() => {
               this.questionWrapper.classList.add('start');
-              this.toggleFall();
               this.playGame();
             }, 300);
           }
@@ -246,6 +315,24 @@ export default class Savanna {
   }
 
   checkAnswer(target) {
+    console.log(this.question)
+    const arr = {
+      audio: "files/02_0030.mp3",
+      audioExample: "files/02_0030_example.mp3",
+      audioMeaning: "files/02_0030_meaning.mp3",
+      group: 0,
+      id: "5e9f5ee35eb9e72bc21af4bd",
+      image: "files/02_0030.jpg",
+      page: 1,
+      textExample: "The sound of her <b>laugh</b> filled the room.",
+      textExampleTranslate: "Звук ее смеха заполнил комнату",
+      textMeaning: "<i>Laugh</i> is the sound made when someone is happy or a funny thing occurs.",
+      textMeaningTranslate: "Смех - это звук, который звучит, когда кто-то счастлив или происходит смешная вещь",
+      transcription: "[læf]",
+      word: "laugh",
+      wordTranslate: "смех",
+      wordsPerExampleSentence: 8
+    }
     const answer = target.innerHTML;
     this.questionWrapper.classList.remove('fall');
     this.gameOn = false;
@@ -261,6 +348,7 @@ export default class Savanna {
       }, 400);
     } else {
       this.currentAnswer.classList.add('answer--false');
+      this.allAnswers[this.answersIndex].parentNode.classList.add('answer--true');
       this.getWrongAnswer();
 
       if (this.errors === 5) {
@@ -277,58 +365,17 @@ export default class Savanna {
   }
 
   showGame() {
-    this.gameContent.classList.toggle('none');
+    this.gameContent.classList.toggle('none'); 
   }
 
   showResults() {
+    this.gameOn = false;
     this.results.classList.toggle('none');
     this.gameOverSound.play();
     this.showGame();
   }
 
-  initGame() {
-    setTimeout(() => {
-      this.gameOn = true;
-      this.generateGameData();
-      this.showGame();
-
-      setTimeout(() => {
-        this.toggleFall();
-        this.checkOffset();
-      }, 600);
-    }, 4000);
-  }
-
-  toggleMenuIcon() {
-    const menu = document.querySelector();
-  }
-
-  start() {
-    this.element = this.getElement();
-    this.startButton = this.element.querySelector('.into__button');
-    this.intro = this.element.querySelector('.intro');
-    this.loader = this.element.querySelector('.loader');
-    this.counter = this.element.querySelector('.preloader__counter');
-    this.sound = this.element.querySelector('.sound');
-    this.answerWords = this.element.querySelectorAll('.answer__word');
-    this.questionWord = this.element.querySelector('.question__word');
-    this.questionWrapper = this.element.querySelector('.question');
-    this.blockAnswers = this.element.querySelector('.answers');
-    this.gameContent = this.element.querySelector('.game-wrapper');
-    this.lives = [...this.element.querySelectorAll('.heart')];
-    this.results = this.element.querySelector('.results');
-
-    this.startButton.addEventListener('click', () => {
-      this.intro.classList.toggle('none');
-      this.loader.classList.toggle('none');
-
-      this.counterOn = true;
-      this.tickPlay(this.playGame);
-      this.startBell.play();
-
-      this.initGame();
-    });
-
+  initHandlers() {
     this.sound.addEventListener('click', () => {
       this.toggleSoundState();
     });
