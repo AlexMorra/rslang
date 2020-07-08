f737de169c4aba86fbb6e29d714b83a2b3cf0857
Eugenia Shibkova <shibkova91@gmail.com>
2020-07-08 03:38:53 +0300

    feat: add team images contants

--- src/js/team/teamConstants.js
+++ src/js/team/teamConstants.js
@@ -1,39 +1,70 @@
 const TEAM = [
   {
-    image: '',
+    image: 'unit-0.jpg',
     title: 'тимлид',
     fullname: 'Александр Картынник',
-    github: 'AlexMorra'
+    github: 'AlexMorra',
+    telegram: 'r17411',
+    features: [
+      'архитектура приложения',
+      'долгосрочная статистика',
+      'словарь',
+      'тренировка слов',
+      'настройки приложения',
+      'авторизация',
+      'основные методы работы со словами'
+    ]
   },
   {
-    image: '',
+    image: 'unit-1.jpg',
     title: 'разработчик',
     fullname: 'Дмитрий Доброгост',
-    github: 'DzmitryDabrahost'
+    github: 'DzmitryDabrahost',
+    telegram: 'dzmitryDabrahost',
+    features: ['игра "Найди пару"']
   },
   {
-    image: '',
+    image: 'unit-2.jpg',
     title: 'разработчик',
     fullname: 'Станислав Яркин',
-    github: 'yarkin13'
+    github: 'yarkin13',
+    telegram: 'yarkin133',
+    features: [
+      'игра "Скажи это"',
+      'игра "Конструктор фраз"',
+      'статистика мини-игр'
+    ]
   },
   {
-    image: '',
+    image: 'unit-3.jpg',
     title: 'разработчик',
     fullname: 'Евгения Шибкова',
-    github: 'jenia-shibkova'
+    github: 'jenia-shibkova',
+    telegram: 'Jenia_Shibkova',
+    features: [
+      'UI приложения',
+      'игра "Саванна"',
+      'промо-страница',
+      'страница игр',
+      'страница о команде',
+      'стартовая мини-игр'
+    ]
   },
   {
-    image: '',
+    image: 'unit-4.jpg',
     title: 'разработчик',
     fullname: 'Алексей Ивкин',
-    github: 'ivkin-alexey'
+    github: 'ivkin-alexey',
+    telegram: 'Ivkin_Aleksey',
+    features: ['игра "Спринт"']
   },
   {
-    image: '',
+    image: 'unit-5.jpg',
     title: 'разработчик',
     fullname: 'Денис Марытчак',
-    github: 'SonOfGlory'
+    github: 'SonOfGlory',
+    telegram: 'denysmarytchak',
+    features: ['игра "Аудиовызов"']
   }
 ];
 
